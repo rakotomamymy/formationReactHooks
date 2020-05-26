1803c6e80bd80cb4d39d793cfbb9beacaee1d404
USER <rakotomamymy@gmail.com>
2020-05-26 16:14:40 +0200

    Move LogOut to an independent component

--- src/App.js
+++ src/App.js
@@ -2,43 +2,49 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Login from './Login/Login';
 import Home from './Home/Home';
-import { BrowserRouter as Router, Route , useHistory} from 'react-router-dom';
+import {LogOutWithHistory} from './LogOut/LogOut';
+import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
 
 function App() {
   
   const [login, setLogin] = useState('Anonymous');
-  const history = useHistory();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem('isAuthenticated') === 'true') {
+      setIsAuthenticated(true);
       setLogin(localStorage.getItem('login'));
+    } else {
+      setIsAuthenticated(false);
+      setLogin('Anonymous');
     }
   }, []);
 
   const userLogged = e => {
+    setIsAuthenticated(true);
     setLogin(e);
   };
 
-  const logingOut = () => {
-    localStorage.removeItem('isAuthenticated');
-    localStorage.removeItem('login');
+  const userLoggedOut = () => {
+    setIsAuthenticated(false);
     setLogin('Anonymous');
-    return history.push('/');
   };
 
   return (
     <div className="container">
       <div>
         <span className="m-3">Connected user : {login}</span>
-        <button className="btn btn-default" onClick={logingOut}>Log out</button>
       </div>
       <Router>
-        <Route exact path="/">
-          <Login onLogginSuccess={userLogged}/>
-        </Route>
-        <Route exact path="/Home/">
-          <Home/>
-        </Route>
+        {isAuthenticated && <LogOutWithHistory logginOut={userLoggedOut}/>}
+        <Switch>
+          <Route path="/Home/">
+            <Home/>
+          </Route>
+          <Route path="/">
+            <Login onLogginSuccess={userLogged}/>
+          </Route>
+        </Switch>        
       </Router>
     </div>    
   );
---
+++ src/LogOut/LogOut.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import { useHistory } from 'react-router-dom';
+
+export default function LogOut(props) {
+
+    const history = useHistory();
+
+    const loggingOut = () => {
+        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('login');
+
+        props.logginOut();
+
+        history.push('/');
+    };
+
+    return(
+        <button className='btn btn-default' onClick={loggingOut}>Log out</button>
+    );
+}
+
+export const LogOutWithHistory = withRouter(LogOut);
